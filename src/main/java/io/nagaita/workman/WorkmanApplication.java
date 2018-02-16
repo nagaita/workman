91c7ce300f54843065a34a88ba88623c51154d5e
Takuma Nagai <tn00gm@gmail.com>
2018-02-16 12:59:41 +0900

    Close #2 Create simple root page.

---
+++ src/main/java/io/nagaita/workman/WorkmanApplication.java
@@ -0,0 +1,12 @@
+package io.nagaita.workman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmanApplication.class, args);
+	}
+}
